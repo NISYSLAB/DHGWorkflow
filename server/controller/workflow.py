384ee161fb5b8a8aca1ee2ee9063007f4010230a
Shubham Awasthi <aw.shubh@gmail.com>
2021-08-28 00:28:39 +0530

    Routes for server

---
+++ server/controller/workflow.py
@@ -0,0 +1,41 @@
+from model.workflows import *
+from flask import request, make_response, Blueprint
+import xml.etree.ElementTree as ET
+
+workFlow = Blueprint('workflow', __name__)
+workFlowModel = WorkFlowModel()
+
+
+@workFlow.route("/", methods=['POST'])
+def postWorkflow():
+    try:
+        ET.fromstring(request.data)
+    except:
+        return "Invalid GraphML", 400
+    graphML = request.data.decode('utf')
+    return workFlowModel.insert(graphML)
+
+
+@workFlow.route("/<id>")
+def getWorkflow(id):
+    res = workFlowModel.get(id)
+    if not res:
+        "Not Found", 404
+    r = make_response(res["graphml"])
+    r.headers.set('x-write-time', str(res["writeTime"]))
+    r.headers.set('Content-Type', "application/xml")
+    return r
+
+
+@workFlow.route("/<id>", methods=['POST'])
+def updateWorkflow(id):
+    try:
+        ET.fromstring(request.data)
+    except:
+        return "Invalid GraphML", 400
+    graphML = request.data.decode('utf')
+    writeTime = float(request.headers['x-write-time'])
+    newWriteTime = workFlowModel.update(id, writeTime, graphML)
+    if not newWriteTime:
+        return "WriteTime or GraphID do not match", 400
+    return str(newWriteTime)
--- server/server.py
+++ server/server.py
@@ -1,19 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+from controller.workflow import workFlow
 from model.workflows import *
 from flask import Flask
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
 app = Flask(__name__)
-workFlowModel = WorkFlowModel()
+app.register_blueprint(workFlow, url_prefix='/workflow')
 
 @app.route("/")
 def Home():
     return "DHGWorkflow"
-    
-@app.route("/workflow/<id>")
-def getWorkflow(id):
-    return workFlowModel.get(id) or "Not Found"
 
 if __name__ == '__main__':
     app.run(port=os.getenv('PORT'), debug=True)
