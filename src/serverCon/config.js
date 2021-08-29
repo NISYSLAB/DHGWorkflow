c25b47b8b22b52392a4784e4ac3cddeb619d4e93
Shubham Awasthi <aw.shubh@gmail.com>
2021-08-29 21:34:58 +0530

    Ajax requests to communicate server

---
+++ src/serverCon/config.js
@@ -0,0 +1,33 @@
+export default {
+    baseURL: 'http://localhost:8000/',
+    getGraph: (id) => `workflow/${id}`,
+    postGraph: 'workflow',
+    updateGraph: (id) => `workflow/${id}`,
+};
+
+/*
+getGraph Route Specs:
+    Method: GET
+    Response:
+        Body: json {writeTime: '23435444.54545', id: 'graphid'}
+        BodyType: application/json
+        Header: X-Write-Time: 'writeTime'
+
+postGraph Route Specs:
+    Method: POST
+    Request:
+        Body: graphML data
+        BodyType: application/xml
+    Response:
+        Body: json {writeTime: '23435444.54545', id: 'graphid'}
+        BodyType: application/json
+
+updateGraph:
+    Method: POST
+    Request:
+        Header: X-Write-Time: 'writeTime'
+        Body: graphML data
+        BodyType: application/xml
+    Response:
+        Body: newWriteTime
+*/
---
+++ src/serverCon/crud_http.js
@@ -0,0 +1,43 @@
+import ec from './config';
+
+function getGraph(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`${ec.baseURL + ec.getGraph(id)}`).then((x) => {
+            resolve(x);
+        }).catch((e) => reject(e));
+    });
+}
+
+function postGraph(graphml) {
+    return new Promise((resolve, reject) => {
+        fetch(`${ec.baseURL + ec.postGraph}/`, {
+            headers: {
+                'Content-Type': 'application/xml',
+            },
+            method: 'POST',
+            body: graphml,
+
+        }).then((x) => {
+            resolve(x);
+        }).catch((e) => reject(e));
+    });
+}
+
+function updateGraph(id, graphml, writeTime) {
+    return new Promise((resolve, reject) => {
+        fetch(ec.baseURL + ec.updateGraph(id), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/xml',
+                'X-Write-Time': writeTime,
+            },
+            body: graphml,
+        }).then((x) => {
+            resolve(x);
+        }).catch((e) => reject(e));
+    });
+}
+
+export {
+    getGraph, postGraph, updateGraph,
+};
